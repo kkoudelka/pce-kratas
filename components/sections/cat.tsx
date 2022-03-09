a01a3ce0d11b82c31c773a23fae986c9bf44a3df
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2022-03-09 23:30:31 +0000

    Registration

--- components/sections/cat.tsx
+++ components/sections/cat.tsx
@@ -22,7 +22,7 @@ const Categories: React.FC = () => {
             <li>Krátký hraný film</li>
             <li>Animovaný film</li>
             <li>Videoklip a YouTube videoprezentace</li>
-            <li>Juniorské filmové okénko (pro žáky 2. stupně ZŠ)</li>{" "}
+            <li>Juniorské filmové okénko (pro žáky 2. stupně ZŠ)</li>
             <li>Snímky studentů filmových škol do 21 let</li>
           </ul>
         </div>
@@ -40,23 +40,78 @@ const Categories: React.FC = () => {
             například <em>poukazy FOMEI</em> a <em>foto/video techniku</em>,
             která se rozhodně neztratí.
           </p>
+        </div>
 
+        <div className="d-flex">
+          <h2 className={classNames("text-left my-3", styles.title)}>
+            Vyhlášení
+          </h2>
+        </div>
+        <p>
+          Projekce a slavnostní vyhlášení soutěžních filmů budou probíhat online
+          prostřednictvím videostreamu na našem YouTube kanálu{" "}
+          <a
+            href="https://www.youtube.com/channel/UCCTJGW3hZ7VjCr4LrDVtyQg"
+            target="_blank"
+          >
+            Delta Film
+          </a>
+          .
+        </p>
+
+        <hr className="my-5" />
+
+        <div className="carticka prihlaska">
           <div className="d-flex">
             <h2 className={classNames("text-left my-3", styles.title)}>
-              Vyhlášení
+              Jak se přihlásit?
             </h2>
           </div>
           <p>
-            Projekce a slavnostní vyhlášení soutěžních filmů budou probíhat
-            online prostřednictvím videostreamu na našem videokanálu{" "}
-            <a
-              href="https://www.youtube.com/channel/UCCTJGW3hZ7VjCr4LrDVtyQg"
-              target="_blank"
-            >
-              Delta Film
-            </a>
-            .
+            Přihlášení na Pardubický kraťas ti zabere jenom chvíli! Stačí
+            následovat tři jednoduché kroky:
           </p>
+          <ol>
+            <li>
+              <h3 className="underline">Vyplň soutěžní přihlášku!</h3>
+              <p>
+                Zde nalezneš odkaz ke{" "}
+                <a
+                  href="https://pardubickykratas.cz/kratas_prihlaska.pdf"
+                  target="_blank"
+                >
+                  stažení přihlášky
+                </a>
+                .
+              </p>
+            </li>
+
+            <li>
+              <h3 className="underline">Nahraj svůj hotový film online!</h3>
+              <p>
+                Nahraj finální podobu filmu na internet - například na porál{" "}
+                <a href="https://www.uschovna.cz/" target="_blank">
+                  Úschovna
+                </a>
+                , nebo{" "}
+                <a href="https://wetransfer.com" target="_blank">
+                  WeTransfer
+                </a>
+                .
+              </p>
+            </li>
+
+            <li>
+              <h3 className="underline">Pošli nám odkaz!</h3>
+              <p>
+                Zašli nám odkaz na platformu s tvým dílem na emailovou adresu{" "}
+                <a href="mailto:info@pardubickykratas.cz" target="_blank">
+                  info@pardubickykratas.cz
+                </a>
+                .
+              </p>
+            </li>
+          </ol>
         </div>
       </div>
     </section>
